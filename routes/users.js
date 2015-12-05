0952df33df0ae6cee3e17357da87630b6b74db79
GaoQ <18912936976@163.com>
2015-12-05 18:34:34 +0800

    20151205

---
+++ routes/users.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/reg', function(req, res, next) {
+  res.render('user/reg',{});
+});
+
+router.post('/reg', function(req, res, next) {
+    var user =  req.body;//读取用户提交过来的注册表单
+    new Model('User')(user).save(function(err,user){
+        if(err){
+            res.redirect('/users/reg');
+        }else{
+            res.redirect('/users/login');
+        }
+    });
+});
+
+router.post('/login', function(req, res, next) {
+    var user =  req.body;
+    Model('User').findOne(user,function(err,user){
+        if(err){
+            res.redirect('/users/login');
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('user/login',{});
+});
+
+router.post('/login', function(req, res, next) {
+  res.send('注册');
+});
+
+router.get('/logout', function(req, res, next) {
+  res.send('退出');
+});
+
+module.exports = router;
